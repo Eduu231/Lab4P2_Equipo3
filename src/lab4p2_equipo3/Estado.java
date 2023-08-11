739df3072fb607acd069c7b28197baeecc5df36e
Alejandro Cardona <alexcardona3002@gmail.com>
2023-08-11 14:02:31 -0600

    Prueba 3

---
+++ src/lab4p2_equipo3/Estado.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4p2_equipo3;
+
+/**
+ *
+ * @author jets
+ */
+public class Estado {
+    
+    private int lmao;
+}
