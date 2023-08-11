8c970005c71ccb1b7d1252d7c74cce60b753abe7
Eduardo Chinchilla <edutrm123@gmail.com>
2023-08-11 13:51:15 -0600

    creacion de proyecto y clases

--- src/lab4p2_equipo3/Entrenador.java
+++ src/lab4p2_equipo3/Entrenador.java
@@ -9,6 +9,16 @@ public class Entrenador {
     private String nombre;
     private int edad;
     private double dinero;
-    private int [] equipo ;
+    private String [] equipo ;
     private ArrayList caja;
+
+    public Entrenador(String nombre, int edad, double dinero, String[] equipo, ArrayList caja) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dinero = dinero;
+        this.equipo = equipo;
+        this.caja = caja;
+    }
+    
+    
 }
